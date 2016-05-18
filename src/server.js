a2d71d7399a33e6c395e0bec6e70f1bf591a928b
Lucas Barros <lucsbarros@gmail.com>
2016-05-17 21:56:28 -0300

    fix(server): update server script

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('../webpack.config');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../webpack.config');
 
-var app = new (require('express'))();
-var port = 3000;
+const app = new (require('express'))();
+const port = 3000;
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
 
